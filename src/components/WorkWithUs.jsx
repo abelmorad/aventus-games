4303471f76508151005aa8bfa4ca44a40751f605
abelmorad <abelmorad97@gmail.com>
2023-05-29 14:50:08 +0800

    New card compoenent

---
+++ src/components/WorkWithUs.jsx
@@ -0,0 +1,8 @@
+export default function WorkWithUs() {
+    return (
+        <div className="work-with-us-card">
+            <h2>lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum maiores ratione in fugit cumque enim quidem vel, quam, fugiat expedita quae distinctio at deserunt?</p>
+        </div>
+    )
+}
