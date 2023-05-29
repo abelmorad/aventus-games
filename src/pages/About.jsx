016a6162dcfc75d1f9b4697e0bb3b370f51579ad
abelmorad <abelmorad97@gmail.com>
2023-05-29 17:46:59 +0800

    About page layout finish

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,15 @@ import heroImg1 from '../../public/images/about/game1.svg'
 import companyImg from '../../public/images/about/company.svg'
 import WorkWithUs from '../components/WorkWithUs'
 
+import OurTeamCard from '../components/OurTeamCard'
+import abel from '../../public/images/about/abel.svg'
+import employee2 from '../../public/images/about/employee2.svg'
+import employee3 from '../../public/images/about/employee3.svg'
+import employee4 from '../../public/images/about/employee4.svg'
+import InputEmail from '../components/InputEmail'
+import Subscribe from '../components/Subscribe'
+import Footer from '../components/Footer'
+
 export default function About() {
     return (
         <>
@@ -32,7 +41,27 @@ export default function About() {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur hic soluta possimus velit assumenda, dignissimos ipsa obcaecati magni corporis nisi pariatur dolores neque, quia alias debitis sapiente. Deserunt, molestiae?</p>
                     </div>
                 </section>
+                <section className="our-team-container">
+                    <h2>Our team</h2>
+                    <div className='our-team-wrapper'>
+                        <OurTeamCard 
+                            profilePicture={abel}
+                            employee={"Abel Morad"}
+                            position={"CEO"} />
+                        <OurTeamCard profilePicture={employee2}
+                            employee={"Malik Tom"}
+                            position={"COO"}/>
+                        <OurTeamCard profilePicture={employee3}
+                            employee={"Sittie Nor"}
+                            position={"CFO"}/>
+                        <OurTeamCard profilePicture={employee4}
+                            employee={"Janeen Fylx"}
+                            position={"CTO"}/>
+                    </div>
+                </section>
             </main>
+            <Subscribe />
+            <Footer />
         </>
     )
 }
